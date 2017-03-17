0a38ca716f65f1902842b6277ec688215ca9dfb7
Balancingrock <github@balancingrock.nl>
2017-03-17 10:47:43 +0100

    Added paths for console ssl

--- Sources/FileURLs.swift
+++ Sources/FileURLs.swift
@@ -3,7 +3,7 @@
 //  File:       FileURLs.swift
 //  Project:    Swiftfire
 //
-//  Version:    0.9.15
+//  Version:    0.9.17
 //
 //  Author:     Marinus van der Lugt
 //  Company:    http://balancingrock.nl
@@ -22,9 +22,8 @@
 //
 //  I also ask you to please leave this header with the source code.
 //
-//  I strongly believe that the Non Agression Principle is the way for societies to function optimally. I thus reject
-//  the implicit use of force to extract payment. Since I cannot negotiate with you about the price of this code, I
-//  have choosen to leave it up to you to determine its price. You pay me whatever you think this code is worth to you.
+//  I strongly believe that voluntarism is the way for societies to function optimally. Thus I have choosen to leave it
+//  up to you to determine the price for this code. You pay me whatever you think this code is worth to you.
 //
 //   - You can send payment via paypal to: sales@balancingrock.nl
 //   - Or wire bitcoins to: 1GacSREBxPy1yskLMc9de2nofNv2SNdwqH
@@ -49,6 +48,7 @@
 //
 // History
 //
+// 0.9.17  - Added paths for secure console connections
 // 0.9.15  - General update and switch to frameworks
 // 0.9.14  - Added serverBlacklist
 //         - Upgraded to Xcode 8 beta 6
@@ -64,9 +64,14 @@
 // Description
 // =====================================================================================================================
 //
-// By default all files and directories are relative to the root directory (which is by default the app support directory) in "~/Library/Application Support/Swiftfire".
+// All files and directories are relative to the root directory in "~/Library/Application Support/Swiftfire".
 //
 // root
+// - ssl
+//   - trusted-console-certificates
+//   - console
+//     - certificate.pem
+//     - private-key.pem
 // - domains
 //   - domain-defaults.json
 //   - ... subdirectories for each domain
@@ -141,6 +146,32 @@ final class FileURLs {
             
         } catch { return nil }
     }()
+    
+    
+    // =================================================================================================================
+    /// Ssl support directory
+    
+    static var sslDir: URL? = { dirUrl(rootDir, "ssl") }()
+    
+    
+    /// The directory with trusted console certificates
+    
+    static var sslTrustedConsoleCertificatesDir: URL? = { dirUrl(sslDir, "trusted-console-certificates") }()
+    
+    
+    /// The directory with certificate and key for the console connection
+    
+    static var sslConsoleDir: URL? = { dirUrl(sslDir, "console") }()
+    
+    
+    /// The certificate to be used for the console connection
+    
+    static var sslConsoleCertificateFile: URL? = { fileUrl(sslConsoleDir, "certificate.pem") }()
+
+    
+    /// The private key to be used for the console connection
+    
+    static var sslConsolePrivateKeyFile: URL? = { fileUrl(sslConsoleDir, "privateKey.pem") }()
     
     
     // =================================================================================================================
