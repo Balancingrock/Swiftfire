c3ce617d768d940d125eef03a3441d55c7057a7f
Balancingrock <github@balancingrock.nl>
2017-03-18 16:40:47 +0100

    Updated for SwifterCore 0.9.17

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Swiftfire",
     dependencies: [
-        .Package(url: "../SwiftfireCore", Version(0,  9,  17))
+        .Package(url: "https://github.com/Balancingrock/SwiftfireCore", Version(0,  9,  17))
     ]
 )
