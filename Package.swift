e3f7b7721de142c38c8a515ce27a7cf3c8118e4b
Balancingrock <github@balancingrock.nl>
2017-04-13 14:37:27 +0200

    SwiftfireCore updated to 0.10.6

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Swiftfire",
     dependencies: [
-        .Package(url: "https://github.com/Balancingrock/SwiftfireCore", Version(0, 10, 5)),
+        .Package(url: "../SwiftfireCore", Version(0, 10, 6)),
         .Package(url: "https://github.com/Balancingrock/KeyedCache", Version(0, 1, 0))
     ]
 )
