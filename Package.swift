14595d31dfa21bf96484eb90346c175774201a50
Balancingrock <github@balancingrock.nl>
2017-05-24 14:23:04 +0200

    Update of dependency versions

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "Swiftfire",
     dependencies: [
-        .Package(url: "https://github.com/Balancingrock/SwifterLog", Version(0, 10, 9)),
-        .Package(url: "https://github.com/Balancingrock/SecureSockets", Version(0, 4, 7)),
-        .Package(url: "https://github.com/Balancingrock/KeyedCache", Version(0, 2, 0))
+        .Package(url: "https://github.com/Balancingrock/SwifterLog", Version(0, 10, 10)),
+        .Package(url: "https://github.com/Balancingrock/SecureSockets", Version(0, 4, 8)),
+        .Package(url: "https://github.com/Balancingrock/KeyedCache", Version(0, 3, 0))
     ]
 )
