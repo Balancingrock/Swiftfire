79df3c0f9a71d72615d6fe2778e234e72168ac67
Balancingrock <github@balancingrock.nl>
2017-05-28 19:48:54 +0200

    Statistics partly implemented

--- Sources/Function.SF.StatisticsPage.swift
+++ Sources/Function.SF.StatisticsPage.swift
@@ -75,6 +75,10 @@
 // session = environment.serviceInfo[.sessionKey] // Must be a non-expired session.
 // session[.accountKey] must contain an admin account
 //
+// Class of the div containing the 'Switch to...' button: statistics-switch-div
+// Class of the table for the domains: statistics-domains-table
+// Class of the table for the clients: statistics-clients-table
+//
 //
 // Returns:
 // --------
@@ -102,6 +106,40 @@ import Foundation
 
 func function_sf_statisticsPage(_ args: Function.Arguments, _ info: inout Function.Info, _ environment: inout Function.Environment) -> Data? {
     
+    struct Button: CustomStringConvertible {
+        var title: String
+        var description: String {
+            return "<input type=\"submit\" value=\"\(title)\">"
+        }
+        init(_ title: String) {
+            self.title = title
+        }
+    }
+    
+    struct Hidden: CustomStringConvertible {
+        var name: String
+        var value: String
+        var description: String {
+            return "<input type=\"hidden\" name=\"\(name)\" value=\"\(value)\">"
+        }
+        init(_ name: String, _ value: String) {
+            self.name = name
+            self.value = value
+        }
+    }
+    
+    struct DomainForm: CustomStringConvertible {
+        var content: String
+        var title: String
+        var description: String {
+            return "<form method=\"post\" action=\"/serveradmin/pages/statistics.sf.html\">\(Hidden("RequestedPage", "Domains"))\(content)\(Button(title))</form>"
+        }
+        init(_ title: String, content: String) {
+            self.title = title
+            self.content = content
+        }
+    }
+    
     
     // Check access rights
     
@@ -122,18 +160,165 @@ func function_sf_statisticsPage(_ args: Function.Arguments, _ info: inout Functi
     
     let postInfo = environment.serviceInfo[.postInfoKey] as? PostInfo ?? PostInfo()
     
+    var result: String = ""
+
+    
     // Domains or Clients?
     
-    let showClients: Bool = postInfo["RequestedPage"] == "Clients"
-    
-    // Create the page
-    
-    var result: String = ""
-    
-    
-    
-    
-    
+    if postInfo["RequestedPage"] == "Domains" {
+        
+        // Show the list of domains
+        // ------------------------
+        
+        // Button to select domains or clients list
+        // ----------------------------------------
+        
+        result += "<h2 style=\"text-align:center;\">URL Visits</h2><div class=\"statistics-switch-div\"><form action=\"/serveradmin/pages/statistics.sf.html\" method=\"post\">\(Hidden("RequestedPage", "Clients"))\(Button("Switch to Client View"))</form></div>"
+        
+        // The domains list
+        // ----------------
+        
+        if let domainName = postInfo["Domain"] {
+            
+            var part = statistics.domains.getDomain(for: domainName)
+            if part != nil {
+                
+                var partName = domainName
+                
+                while part != nil {
+                    let nextPartName = postInfo[partName]
+                    if nextPartName == nil { break }
+                    let nextPart = part!.getPathPart(for: nextPartName!)
+                    if nextPart == nil { break }
+                    part = nextPart
+                    partName = nextPartName!
+                }
+                
+                // Add the table header
+                
+                var backButtonCode: String = ""
+                if part?.previous != nil {
+                    
+                    backButtonCode = "\(Hidden("Domain", domainName))"
+                    
+                    var pathPart = domainName
+                    while true {
+                        if let nextPathPart = postInfo[pathPart] {
+                            if postInfo[nextPathPart] != nil {
+                                backButtonCode += Hidden(pathPart, nextPathPart).description
+                            }
+                            pathPart = nextPathPart
+                        } else {
+                            break
+                        }
+                    }
+
+                    backButtonCode = DomainForm("Back", content: backButtonCode).description
+                    
+                } else {
+                    backButtonCode = DomainForm("Back", content: "").description
+                }
+                
+                result += "<table class=\"statistics-domains-table\"><thead><tr><th colspan=\"3\"><div>URL: \(part!.fullUrl)\(backButtonCode)</div></th></tr><tr><th>Accesses</th><th>URL Part</th><th>Do Not Trace</th></tr></thead><tbody>"
+                
+                
+                // Ad the rows
+                
+                for row in part!.nextParts {
+                    
+                    
+                    // Create the path button postInfo
+                    
+                    var pathCode = "\(Hidden("RequestedPage", "Domains"))\(Hidden("Domain", domainName))"
+                    
+                    var pathPart = domainName
+                    while true {
+                        if let nextPathPart = postInfo[pathPart] {
+                            pathCode += Hidden(pathPart, nextPathPart).description
+                            pathPart = nextPathPart
+                        } else {
+                            pathCode += Hidden(pathPart, row.pathPart).description
+                            break
+                        }
+                    }
+                    
+                    
+                    // Find out if this row represents a dictionary
+                    
+                    var components = URL(string: row.fullUrl)?.pathComponents
+                    let urlDomainName = components?.remove(at: 0)
+                    let domain = domains.domain(forName: urlDomainName)!
+                    var url = URL(fileURLWithPath: domain.root)
+                    components?.forEach({ url = url.appendingPathComponent($0) })
+                    var isDir: ObjCBool = false
+                    (environment.connection as? SFConnection)?.filemanager.fileExists(atPath: url.path, isDirectory: &isDir)
+                    
+                    // Create 'details' button
+                    
+                    var details: String = ""
+                    if isDir.boolValue { details += DomainForm("Detail", content: pathCode).description }
+                    
+                    
+                    result += "<tr><td>\(row.foreverCount)</td><td><div>\(row.pathPart)\(details)</div></td><td>\(row.doNotTrace ? "true" : "false")</td></tr>"
+                }
+                
+                
+                // Add the end of table
+                
+                result += "</tbody></table>"
+
+            } else {
+                
+                result += "***Error***"
+            }
+            
+        } else {
+            
+            // Add the table header
+            
+            result += "<table class=\"statistics-domains-table\"><thead><tr><th>Accesses</th><th>URL Part</th><th>Do Not Trace</th></tr></thead><tbody>"
+            
+            
+            // Ad the rows
+            
+            for row in statistics.domains.domains {
+                
+                result += "<tr><td>\(row.foreverCount)</td><td><div>\(row.pathPart)\(DomainForm("Detail", content: Hidden("Domain", row.pathPart).description))</div></td><td>\(row.doNotTrace ? "true" : "false")</td></tr>"
+            }
+            
+            
+            // Add the end of table
+            
+            result += "</tbody></table>"
+        }
+        
+        
+    } else {
+        
+        // Show the list of clients
+        
+        result += "<h2 style=\"text-align:center;\">Client visits</h2><div class=\"statistics-switch-div\"><form action=\"/serveradmin/pages/statistics.sf.html\" method=\"post\"><input type=\"hidden\" name=\"RequestedPage\" value=\"Domains\"><input type=\"submit\" value=\"Switch to URL View\"></form></div>"
+        
+        
+        // The client list
+        
+        // Add the table header
+        
+        result += "<table class=\"statistics-clients-table\"><thead><tr><th>Address</th><th>Count</th><th>Do Not Trace</th></tr></thead><tbody>"
+
+        // Ad the rows
+        
+        for row in statistics.clients.clients {
+            
+            result += "<tr><td>\(row.address)</td><td>\(row.records.count)</td><td>\(row.doNotTrace ? "true" : "false")</td></tr>"
+        }
+        
+        
+        // Add the end of table
+        
+        result += "</tbody></table>"
+    }
+        
     return result.data(using: String.Encoding.utf8)
 }
 
