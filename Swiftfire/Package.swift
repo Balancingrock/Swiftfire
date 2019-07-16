a421b864dec5d10861dce12dd567f53e96dd7f5a
Rien <rien>
2019-07-16 20:26:29 +0200

    updated to proper versions

--- Swiftfire/Package.swift
+++ Swiftfire/Package.swift
@@ -8,11 +8,11 @@ let package = Package(
         .executable(name: "Swiftfire", targets: ["Swiftfire"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Balancingrock/SwifterLog", from: "1.2.0"),
-        .package(url: "https://github.com/Balancingrock/SecureSockets", from: "0.5.0"),
-        .package(url: "https://github.com/Balancingrock/KeyedCache", from: "0.7.0"),
-        .package(url: "https://github.com/Balancingrock/BRBON", from: "0.6.0"),
-        .package(url: "https://github.com/Balancingrock/Http", from: "0.1.0"),
+        .package(url: "https://github.com/Balancingrock/SwifterLog", from: "1.7.0"),
+        .package(url: "https://github.com/Balancingrock/SecureSockets", from: "0.6.0"),
+        .package(url: "https://github.com/Balancingrock/KeyedCache", from: "0.8.0"),
+        .package(url: "https://github.com/Balancingrock/BRBON", from: "0.8.0"),
+        .package(url: "https://github.com/Balancingrock/Http", from: "0.2.1"),
         .package(url: "https://github.com/Balancingrock/Html", from: "0.1.0")
     ],
     targets: [
