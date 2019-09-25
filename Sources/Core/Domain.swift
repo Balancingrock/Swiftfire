b485f3e9d2474ecc2411c7c0f3a979ac30be1c04
rien <github@balancingrock.nl>
2019-09-25 09:14:53 +0200

    Warning from xcode 11 removed

--- Sources/Core/Domain.swift
+++ Sources/Core/Domain.swift
@@ -3,7 +3,7 @@
 //  File:       Domain.swift
 //  Project:    Swiftfire
 //
-//  Version:    1.2.0
+//  Version:    1.3.0
 //
 //  Author:     Marinus van der Lugt
 //  Company:    http://balancingrock.nl
@@ -36,6 +36,7 @@
 //
 // History
 //
+// 1.3.0 - Replaced var with let due to Xcode 11
 // 1.2.0 - Added admin keyword
 //       - Set session timeout to 600 (seconds)
 // 1.1.0 #3 Fixed loading & storing of domain service names
@@ -97,7 +98,7 @@ public final class Domain {
             
             // Split at ':' character
                 
-            var strs = value.components(separatedBy: ":")
+            let strs = value.components(separatedBy: ":")
                 
                 
             // If there is one item, then there is no ':' in the string, the new value is then the address
