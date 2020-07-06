d260fc38825896dfbdb2ad3af87a548dbc26b729
Balancingrock <rien@balancingrock.nl>
2020-07-06 12:17:39 +0200

    fixed build scripts

--- sf-build.sh
+++ sf-build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 
-    OPENSSL = "openssl/v1_1_1g-mint_19_3"
+    OPENSSL_PATH="${PWD}/openssl/v1_1_1g-mint_19_3"
 
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 
-    OPENSSL = "openssl/v1_1_1g-macos_10_15"
+    OPENSSL_PATH="${PWD}/openssl/v1_1_1g-macos_10_15"
 
 else
     echo "Error: Unknown OS"
     exit 1
 fi
 
-echo "Using openssl path: $OPENSSL"
-swift build -Xswiftc -I"$OPENSSL/include" -Xlinker -L"$OPENSSL/lib"
+echo "Using openssl path: $OPENSSL_PATH"
+swift build -Xswiftc -I"${OPENSSL_PATH}/include" -Xlinker -L"${OPENSSL_PATH}/lib"
